6b70cfc74837138c67d48f1960ebbd814da1057d
Dhanashree <68148088+Dhanashree-gaware@users.noreply.github.com>
2020-11-21 13:58:23 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,10 @@
+#Task1
 radius=eval(input("Enter radius of a circle : "))
 area= 3.14*radius**2
 print("The area of circle with radius",radius,"is :",area)
+
+
+#Task2
+filename=input("Enter the filename :")
+extension=filename.split(".")
+print("The extension of the file is :",extension[-1])
